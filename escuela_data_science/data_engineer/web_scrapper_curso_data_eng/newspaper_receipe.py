235309c45ee5791f5cc8d4e7d90a23b92f35bfde
Mauricio Gomez <63175683+maurogome@users.noreply.github.com>
2020-06-12 23:10:05 -0500

    cleaning data receipe

--- escuela_data_science/data_engineer/web_scrapper_curso_data_eng/newspaper_receipe.py
+++ escuela_data_science/data_engineer/web_scrapper_curso_data_eng/newspaper_receipe.py
@@ -1,4 +1,7 @@
 import argparse
+import hashlib
+import nltk
+from nltk.corpus import stopwords
 import logging
 logging.basicConfig(level = logging.INFO)
 from urllib.parse import urlparse
@@ -6,6 +9,8 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
 
 def main(filename):
@@ -15,6 +20,10 @@ def main(filename):
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_lines_from_body(df)
+    df = _tokenize_column(df, ['title', 'body'])
 
     return df
 
@@ -42,6 +51,60 @@ def _extract_host(df):
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
 
     return df
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+    
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                    )
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+    
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uids for each row')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis = 1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+
+    df['uid'] = uids
+
+    return df.set_index('uid')
+
+def _tokenize_column(df, column_name):
+    stop_words = set(stopwords.words('spanish'))
+    for column in column_name:
+        logger.info(f'Tokenizing column {column}')
+        tokenize_columns = (df
+                                .dropna()
+                                .apply(lambda row: nltk.word_tokenize(row[column]), axis = 1)
+                                .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                                .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                                .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                                .apply(lambda valid_word_list: len(valid_word_list))
+                                )
+        df[column] = tokenize_columns
+
+    return df
+
+def _remove_new_lines_from_body(df):
+    logger.info('Removing new lies from body')
+
+    stripped_body = (df
+                    .apply(lambda row: row['body'], axis = 1)
+                    .apply(lambda body: list(body))
+                    .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ' '), letters)))
+                    .apply(lambda letters: ''.join(letters))
+                    )
+    df['body'] = stripped_body
+
+    return df
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
