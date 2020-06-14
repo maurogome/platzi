7f98e1b71c5e2fe32e31a85294d64cb6561fbe8f
Mauricio Gomez <63175683+maurogome@users.noreply.github.com>
2020-06-14 12:53:30 -0500

    organizing data

--- escuela_data_science/data_engineer/web_scrapper_curso_data_eng/pipeline.py
+++ escuela_data_science/data_engineer/web_scrapper_curso_data_eng/pipeline.py
@@ -22,8 +22,8 @@ def _transform():
     logger.info('Starting transform process')
     for news_site_uid in news_sites_uids:
         dirty_data_filename = '{}_.csv'.format(news_site_uid)
-        clean_data_filename = 'clean_{}'.forma(dirty_data_filename)
-        subprocess.run(['python', 'main.py', dirty_data_filename], cwd = './transform')
+        clean_data_filename = 'clean_{}'.format(dirty_data_filename)
+        subprocess.run(['python', 'newspaper_receipe.py', dirty_data_filename], cwd = './transform')
         subprocess.run(['rm', dirty_data_filename], cwd = './transform')
         subprocess.run(['mv', clean_data_filename, '../load/{}.csv'.format(news_site_uid)], cwd = './transform')
     
@@ -33,7 +33,7 @@ def _load():
     for news_site_uid in news_sites_uids:
         clean_data_filename = '{}.csv'.format(news_site_uid)
         subprocess.run(['python', 'main.py', clean_data_filename], cwd = './load')
-        subprocess.run(['rm', clean_data_filename], cwdd = './load')
+        subprocess.run(['rm', clean_data_filename], cwd = './load')
 
 
 if __name__ == '__main__':
