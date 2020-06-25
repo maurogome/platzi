86b6efac3f71ea5a4917e17825c1606fe948257a
Mauricio Gomez <63175683+maurogome@users.noreply.github.com>
2020-06-24 21:38:16 -0500

    web scraping update

--- escuela_data_science/data_engineer/web_scrapper_curso_data_eng/aroesti/web_scrapper_curso_data_eng/newspaper_receipe.py
+++ escuela_data_science/data_engineer/web_scrapper_curso_data_eng/aroesti/web_scrapper_curso_data_eng/newspaper_receipe.py
@@ -1,16 +1,16 @@
 import argparse
 import hashlib
+import logging
+logging.basicConfig(level=logging.INFO)
+from urllib.parse import urlparse
+
+
+import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-import logging
-logging.basicConfig(level = logging.INFO)
-from urllib.parse import urlparse
-import pandas as pd
+
 
 logger = logging.getLogger(__name__)
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
 
 
 def main(filename):
@@ -23,37 +23,42 @@ def main(filename):
     df = _fill_missing_titles(df)
     df = _generate_uids_for_rows(df)
     df = _remove_new_lines_from_body(df)
-    df = _tokenize_column(df, ['title', 'body'])
+    df = _tokenize_column(df, 'title')
+    df = _tokenize_column(df, 'body')
     df = _remove_duplicate_entries(df, 'title')
     df = _drop_rows_with_missing_values(df)
     _save_data(df, filename)
 
     return df
 
-def _read_data(filename):
-    logger.info(f'Reading file {filename}')
 
-    return pd.read_csv(filename, encoding = 'latin')
+def _read_data(filename):
+    logger.info('Reading file {}'.format(filename))
+
+    return pd.read_csv(filename)
+
 
 def _extract_newspaper_uid(filename):
     logger.info('Extracting newspaper uid')
     newspaper_uid = filename.split('_')[0]
 
-    logger.info(f'newspaper uid detected: {newspaper_uid}')
-
+    logger.info('Newspaper uid detected: {}'.format(newspaper_uid))
     return newspaper_uid
 
+
 def _add_newspaper_uid_column(df, newspaper_uid):
-    logger.info(f'Filling newspaper_uid column with {newspaper_uid}')
-    df['newspaper_uid'] = 'newspaper_uid'
+    logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
+    df['newspaper_uid'] = newspaper_uid
 
     return df
+
 
 def _extract_host(df):
     logger.info('Extracting host from urls')
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
 
     return df
+
 
 def _fill_missing_titles(df):
     logger.info('Filling missing titles')
@@ -63,15 +68,17 @@ def _fill_missing_titles(df):
                         .str.extract(r'(?P<missing_titles>[^/]+)$')
                         .applymap(lambda title: title.split('-'))
                         .applymap(lambda title_word_list: ' '.join(title_word_list))
-                    )
+                      )
+
     df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
 
     return df
 
+
 def _generate_uids_for_rows(df):
     logger.info('Generating uids for each row')
     uids = (df
-            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis = 1)
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
             .apply(lambda hash_object: hash_object.hexdigest())
             )
 
@@ -79,61 +86,64 @@ def _generate_uids_for_rows(df):
 
     return df.set_index('uid')
 
-def _tokenize_column(df, column_name):
-    stop_words = set(stopwords.words('spanish'))
-    for column in column_name:
-        logger.info(f'Tokenizing column {column}')
-        df['n_token_' + column] = (df
-                                .dropna()
-                                .apply(lambda row: nltk.word_tokenize(row[column]), axis = 1)
-                                .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
-                                .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
-                                .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
-                                .apply(lambda valid_word_list: len(valid_word_list))
-                                )
-        #df[column] = tokenize_columns
-
-    return df
 
 def _remove_new_lines_from_body(df):
-    logger.info('Removing new lies from body')
+    logger.info('Remove new lines from body')
 
     stripped_body = (df
-                    .apply(lambda row: row['body'], axis = 1)
-                    .apply(lambda body: list(body))
-                    .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ' '), letters)))
-                    .apply(lambda letters: ''.join(letters))
+                     .apply(lambda row: row['body'], axis=1)
+                     .apply(lambda body: list(body))
+                     .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ' '), letters)))
+                     .apply(lambda letters: ''.join(letters))
                     )
+
     df['body'] = stripped_body
 
     return df
 
-def _remove_duplicate_entries(df, column_name):
-    logger.info('Removing duplicate entries')
-    df.drop_duplicates(subset = [column_name], keep = 'first', inplace = True)
+def _tokenize_column(df, column_name):
+    logger.info('Calculating the number of unique tokens in {}'.format(column_name))
+    stop_words = set(stopwords.words('spanish'))
+
+    n_tokens =  (df
+                 .dropna()
+                 .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                 .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                 .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                 .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                 .apply(lambda valid_word_list: len(valid_word_list))
+            )
+
+    df['n_tokens_' + column_name] = n_tokens
 
     return df
 
-def _drop_rows_with_missing_values(df):
-    logger.info('Droping rows with missing values')
 
+def _remove_duplicate_entries(df, column_name):
+    logger.info('Removing duplicate entries')
+    df.drop_duplicates(subset=[column_name], keep='first', inplace=True)
+
+    return df
+
+
+def _drop_rows_with_missing_values(df):
+    logger.info('Dropping rows with missing values')
     return df.dropna()
 
 
 def _save_data(df, filename):
-    clean_filename = f'clean_{filename}'
-    logger.info(f'Saving data at location {clean_filename}')
+    clean_filename = 'clean_{}'.format(filename)
+    logger.info('Saving data at location: {}'.format(clean_filename))
     df.to_csv(clean_filename)
-
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename',
-                        help = 'The path to dirty data',
-                        type = str)
+                        help='The path to the dirty data',
+                        type=str)
 
     args = parser.parse_args()
 
     df = main(args.filename)
-    print(df)
+
